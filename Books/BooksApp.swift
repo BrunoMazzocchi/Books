c8960314be2a2d2623a1da9f094c4dbec3caac5a
Bruno Mazzocchi <bmazzocchi@tribalworldwide.gt>
2025-03-01 11:32:19 -0600

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Bruno Mazzocchi on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
